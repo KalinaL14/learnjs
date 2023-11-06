057b5c2075799bba00fed28ee12b21d357718764
l.kalina <kalinaluba794@gmail.com>
2023-11-06 22:47:44 +0400

    add typeof

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let title = 'title';
-let screens = 'creens';
-let screenPrice = 'screenPrice';
-let rollback = 'rollback';
-let fullPrice = 'fullPrice';
-let adaptive = 'adaptive';
-alert('Warning');
-console.log(screenPrice);
+let title = 'learnjs';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 321;
+let rollback = 2;
+let fullPrice = 1000000;
+let adaptive = true;
+
+console.log(typeof(title), typeof(fullPrice), typeof(adaptive));
+console.log(screens.length);
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/долларов/гривен/юани. Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/долларов/гривен/юани');
+console.log(screens.toLowerCase().split());
+console.log('Процент отката посреднику за работу'+ ' ' + fullPrice * (rollback/100));
+
 
