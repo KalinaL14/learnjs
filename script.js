51fc3f81adf39ef52dbf4ea2400fbb52d8610d7e
l.kalina <kalinaluba794@gmail.com>
2023-11-06 20:57:05 +0400

    Add var

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'title';
+let screens = 'creens';
+let screenPrice = 'screenPrice';
+let rollback = 'rollback';
+let fullPrice = 'fullPrice';
+let adaptive = 'adaptive';
+alert('Warning');
+console.log(screenPrice);
+
