bfadc63178cc2031aec5e3a6662249c9028024d6
l.kalina <kalinaluba794@gmail.com>
2023-11-25 22:51:30 +0400

    Add lesson 4

--- script.js
+++ script.js
@@ -15,16 +15,51 @@ let servicePrice2 = parseInt(prompt('Сколько это будет стоит
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let rollback = 2;
 let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback/100));
-console.log(servicePercentPrice);
 
-if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%');
-} else if (fullPrice >= 15000) {
-    console.log('Даем скидку в 5%');
-} else if (fullPrice >= 0) {
-    console.log('Скидка не предусмотрена');
-} else {
-    console.log('Что-то пошло не так');  
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        return 'Даем скидку в 10%';
+    } else if (price >= 15000) {
+        return 'Даем скидку в 5%';
+    } else if (price >= 0) {
+        return 'Скидка не предусмотрена';
+    } else {
+        return 'Что-то пошло не так';  
+    }
 }
+console.log(getRollbackMessage(fullPrice))
+
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable)
+} 
+
+let getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2
+}
+let allServicePrices = getAllServicePrices
+
+function getFullPrice() {
+   return screenPrice + allServicePrices()
+}
+
+fullPrice = getFullPrice
+
+title = title.replace(/^[^a-zа-яё]*([a-zа-яё])/i, function(m){
+    return m.toUpperCase();
+})
+
+let getServicePercentPrices = function() {
+    return fullPrice() - servicePercentPrice
+}
+console.log(getServicePercentPrices())
+servicePercentPrice = getServicePercentPrices
+
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
+console.log(screens)
+console.log(fullPrice())
+
 
 
