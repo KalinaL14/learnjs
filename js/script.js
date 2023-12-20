791ae43ad81aa12e0df1aa21794ccefc2a8978d1
l.kalina <kalinaluba794@gmail.com>
2023-12-20 09:44:46 +0400

    fix lesson09

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+const title = document.getElementsByTagName('h1')[0].innerText
+const buttons = document.getElementsByClassName('handler_btn')
+const plus = document.querySelector('.screen-btn').textContent
+const itemsPercent = document.querySelectorAll('.other-items.percent')
+const itemsNumber = document.querySelectorAll('.other-items.number')
+const inputRange = document.querySelector('div.rollback input[type=range]') 
+const inputRangeValue = document.querySelector('div.rollback span.range-value')
+const totalInput = document.getElementsByClassName('total-input')
+const screens = document.querySelectorAll('.screen')             
