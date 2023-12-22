a8e10e718d7c0fc1209fd173a2f8fe5963d1e776
l.kalina <kalinaluba794@gmail.com>
2023-12-22 23:30:44 +0400

    delete adv

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ book[3].after(book[2])
 book[4].after(book[2])
 book[5].after(book[2])
 book[4].after(book[3])
+let backgroundImage = document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)'
+let titleBook = book[4].childNodes[1].children[0].innerText = "Книга 3. this и Прототипы Объектов"
+let deleteAdv = document.body.childNodes[5].remove()
 
 
 
-console.log(book)
+console.log(book[0].childNodes[3])
