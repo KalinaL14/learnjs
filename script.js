fafd04ae7105a6bc670a121d60d3272adb0267b6
l.kalina <kalinaluba794@gmail.com>
2023-12-22 09:56:59 +0400

    add 1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use strict'
-const book = document.querySelectorAll('.book')
-book[1].before(book[0])
-book[2].after(book[5])
+let book = document.querySelectorAll('.book')
+book[1].after(book[0])
+book[3].after(book[2])
+book[4].after(book[2])
+book[5].after(book[2])
 book[4].after(book[3])
+
+
+
 console.log(book)
