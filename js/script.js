c4129574e4ac532c814a7dd7746ca1a7361e7f6a
l.kalina <kalinaluba794@gmail.com>
2023-12-22 23:35:30 +0400

    change total

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict'
 const title = document.getElementsByTagName('h1')[0].innerText
-const buttonStart = document.getElementsByClassName('handler_btn').start
-const buttonReset = document.getElementsByClassName('handler_btn').reset
+const buttonStart = document.getElementsByClassName('handler_btn')[0]
+const buttonReset = document.getElementsByClassName('handler_btn')[1]
 const plus = document.querySelector('.screen-btn').textContent
 const itemsPercent = document.querySelectorAll('.other-items.percent')
 const itemsNumber = document.querySelectorAll('.other-items.number')
 const inputRange = document.querySelector('div.rollback input[type=range]') 
 const inputRangeValue = document.querySelector('div.rollback span.range-value')
 const totalInput = document.getElementsByClassName('total-input')
-const total = document.getElementsByClassName('total-input').total
-const totalCount = document.getElementsByClassName('total-input').total-count
-const totalCountOther = document.getElementsByClassName('total-input').total-count-other
-const totalFullCount = document.getElementsByClassName('total-input').total-full-count
-const totalCountRollback = document.getElementsByClassName('total-input').total-count-rollback
-const screens = document.querySelectorAll('.screen')       
+const total = document.getElementsByClassName('total-input')[0]
+const totalCount = document.getElementsByClassName('total-input')[1]
+const totalCountOther = document.getElementsByClassName('total-input')[2]
+const totalFullCount = document.getElementsByClassName('total-input')[3]
+const totalCountRollback = document.getElementsByClassName('total-input')[4]
+const screens = document.querySelectorAll('.screen')
