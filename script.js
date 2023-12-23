deec1d329c02a128e22a2d3798b01ec5e1c14064
l.kalina <kalinaluba794@gmail.com>
2023-12-23 14:09:41 +0400

    Lesson10

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ book[3].after(book[2])
 book[4].after(book[2])
 book[5].after(book[2])
 book[4].after(book[3])
+
 let backgroundImage = document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)'
+
 let titleBook = book[4].childNodes[1].children[0].innerText = "Книга 3. this и Прототипы Объектов"
+
 let deleteAdv = document.body.childNodes[5].remove()
 
+book[0].childNodes[3].children[2].after(book[0].childNodes[3].children[8])
+book[0].childNodes[3].children[9].after(book[0].childNodes[3].children[2])
+book[0].childNodes[3].children[3].after(book[0].childNodes[3].children[2])
+book[0].childNodes[3].children[2].after(book[0].childNodes[3].children[6])
+
+book[5].childNodes[3].children[1].after(book[5].childNodes[3].children[9])
+book[5].childNodes[3].children[5].after(book[5].childNodes[3].children[3])
+book[5].childNodes[3].children[8].after(book[5].childNodes[3].children[6])
 
 
-console.log(book[0].childNodes[3])
+book[2].children[1].insertAdjacentHTML("beforeend", `<li>Глава 8: За пределами ES</li>`)
+book[2].childNodes[3].children[10].after(book[2].childNodes[3].children[9])
